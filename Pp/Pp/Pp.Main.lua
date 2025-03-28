b43a1d221787b0b304201c005fb2317e2349710c
pl0o0 <robloxfruits0111@gmail.com>
2025-03-28 12:02:28 +0900

    Pp.Main.lua

--- Pp/Pp/Pp.Main.lua
+++ Pp/Pp/Pp.Main.lua
@@ -19,9 +19,9 @@ local OpButuicorn = Instance.new("UICorner")
 OpButuicorn.Parent = OpBut
 OpBut.Parent = ScreenGui
 OpBut.Text = "Open" OpBut.TextScaled = true
-OpBut.Size = UDim2.new(0,50,0,50)
+OpBut.Size = UDim2.new(0,70,0,70)
 OpBut.Position = UDim2.new(0.17,0,0.28,0)
-OpBut.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+OpBut.BackgroundColor3 = Color3.fromRGB(170, 0, 0)
 OpBut.BackgroundTransparency = 0.5
 OpBut.TextColor3 = Color3.fromRGB(255, 255, 255)
 OpButuicorn.CornerRadius = UDim.new(0,15)
@@ -99,15 +99,58 @@ But1addStamina.MouseButton1Click:Connect(function()
 	local stamina = 100
 	game:GetService("ReplicatedStorage"):WaitForChild("WorkoutHandler_increaseStaminaEvent"):FireServer(stamina)
 end)
+
 local Warn = Instance.new("TextLabel")
 local Wrnstaui = Instance.new("UICorner")
 Warn.Parent = Frame
 Warn.Position = UDim2.new(0.470,0,0,0)
 Warn.Size = UDim2.new(0,200,0,50)
-Warn.BackgroundColor3 = Color3.fromRGB(85, 0, 0)
+Warn.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
 Warn.BackgroundTransparency = 0.7
 Warn.Text = "DO NOT ADD MANY STAMINS" Warn.TextScaled = true Warn.TextTransparency = 0.2
 Warn.TextColor3 = Color3.fromRGB(255, 255, 255)
 Wrnstaui.Parent = Warn
 Wrnstaui.CornerRadius = UDim.new(0,8)
 Warn.Font = Enum.Font.SourceSansBold
+
+local AutoClick = Instance.new("TextButton")
+local AtUiCor = Instance.new("UICorner")
+AutoClick.Parent = Frame
+AutoClick.Position = UDim2.new(-0,0,0.15,0)
+AutoClick.Size = UDim2.new(0,200,0,50)
+AutoClick.BackgroundColor3 = Color3.fromRGB(85, 0, 0)
+AutoClick.BackgroundTransparency = 0.7
+AutoClick.Text = "AutoClick" AutoClick.TextScaled = true AutoClick.TextTransparency = 0.2
+AutoClick.TextColor3 = Color3.fromRGB(255, 255, 255)
+AtUiCor.Parent = AutoClick
+AtUiCor.CornerRadius = UDim.new(0,8)
+AutoClick.Font = Enum.Font.SourceSansBold
+
+local OnOffLabel = Instance.new("TextLabel")
+local OnOffCor = Instance.new("UICorner")
+OnOffLabel.Parent = Frame
+OnOffLabel.Position = UDim2.new(0.470,0,0.15,0)
+OnOffLabel.Size = UDim2.new(0,50,0,50)
+OnOffLabel.BackgroundColor3 = Color3.fromRGB(0, 255, 0)
+OnOffLabel.BackgroundTransparency = 0.3
+OnOffLabel.Text = "Off" OnOffLabel.TextScaled = true OnOffLabel.TextTransparency = 0.2
+OnOffLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+OnOffCor.Parent = OnOffLabel
+OnOffCor.CornerRadius = UDim.new(0,10)
+OnOffLabel.Font = Enum.Font.SourceSansBold
+
+local False = false
+
+AutoClick.MouseButton1Click:Connect(function()
+	if not False then
+		False = true
+		OnOffLabel.Text = "On"
+		while False do
+		game:GetService("ReplicatedStorage").WorkoutHandler_playerClicked:FireServer()
+		wait(0.1)
+	end
+	else
+		False = false
+		OnOffLabel.Text = "Off"
+	end
+end)
